5a615c0c6c1db8dc26f7f569e505e36cd7575f6e
Jonas Plum <cugu@users.noreply.github.com>
2020-08-02 14:52:15 +0200

    Add label sync data (#9)

---
+++ labels/update-labels.sh
@@ -0,0 +1,18 @@
+# npm install -g github-label-sync
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/artifactcollector
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/artifactlib
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/artifacts
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/artifactsgo
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/assets2package
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/custom-collector
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/documentation
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/elementary
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/elementary-plugins
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/forensicstore
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/fslib
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/go-vss
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/homebrew-tap
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/pyartifacts
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/pyforensicstore
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/pyforensicstore-stix
+github-label-sync --access-token labels.json --access-token xxx forensicanalysis/stixgo
